902e2f45860fbc76fadd0dcf721557f58f0bba94
cervelo0713 <cervelo0713@gmail.com>
2024-08-07 01:43:13 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vec
+    return 0;
+
+}
